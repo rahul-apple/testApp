ffd14604c8c7661a67a1a733200fa9705f940ecf
RAHUL'S MAC MINI <rahulthazhuthala@gmail.com>
2016-09-02 09:27:12 +0530

    Initial Commit

---
+++ testApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testApp
+//
+//  Created by RAHUL'S MAC MINI on 02/09/16.
+//  Copyright © 2016 iDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
